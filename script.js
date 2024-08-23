f421e904f3ea6ca83f596ca96153b1f8e8fa86d5
Davidmetam <125705616+Davidmetam@users.noreply.github.com>
2024-08-22 23:20:34 -0600

    finalizacion del proyecto, se agregan funcionalidades responsivas y funcionamiento con js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Carrousel
 document.addEventListener('DOMContentLoaded', function() {
     const carrusel = document.getElementById('carrusel-intereses');
     const intereses = carrusel.querySelectorAll('.interes');
@@ -26,3 +27,73 @@ document.addEventListener('DOMContentLoaded', function() {
 
     setInterval(moveNext, 2000);
 });
+
+//Mostrar ocultar menu responsivo
+let menuVisible = false;
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList="responsive";
+        menuVisible = true;
+    }
+}
+
+window.onscroll = function(){
+    efectoHabilidades();
+}
+
+//Barras de porcentajes de skills
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javacore");
+        habilidades[1].classList.add("comunicacion");
+        habilidades[2].classList.add("maven");
+        habilidades[3].classList.add("trabajoequipo");
+        habilidades[4].classList.add("hibernate");
+        habilidades[5].classList.add("resoluciondeproblemas");
+        habilidades[6].classList.add("springframework");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("mysql");
+        habilidades[9].classList.add("dedicacion");
+    }
+}
+
+
+//listener para ingresar solo numeros en form / telefono
+document.getElementById('telefono').addEventListener('input', function(event) {
+    this.value = this.value.replace(/\D/g, '');
+});
+
+
+//Validacion de casillas en form de contacto
+document.getElementById('submitBtn').addEventListener('click', function(event) {
+    event.preventDefault(); 
+
+    const fields = [
+        document.getElementById('nombre'),
+        document.getElementById('telefono'),
+        document.getElementById('email'),
+        document.getElementById('tema'),
+        document.getElementById('mensaje')
+    ];
+
+    
+    for (let field of fields) {
+        if (field.value.trim() === '') {
+            alert(`Por favor, completa el campo ${field.placeholder}`);
+            field.focus();
+            return; 
+        }
+    }
+    fields.forEach(field => field.value = '');
+    alert("Formulario enviado exitosamente!");
+    
+});
+
+
+
